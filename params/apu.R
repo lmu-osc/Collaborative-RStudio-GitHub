6754736aeb135a5a154c64576d5af346aea0a7ee
APARAJITHA Ramesh <APARAJITHA Ramesh>
2016-08-03 17:27:32 +0100

    my commit

---
+++ params/apu.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <-0.9
+    
+# e.g. "anas_krystallinus"    
+species.name <-mr_apulopus
+    
+# e.g. "red"    
+color <- "cyan"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
