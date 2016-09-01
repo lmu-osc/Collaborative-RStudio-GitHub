ac032dd6e7a850826181b4fb0c3e28867ac4bf6b
Alejandro Gonzalez Voyer <alejandro.gonzalez@iecologia.unam.mx>
2016-09-01 14:14:03 -0500

    Added parameters for a_voyerii

---
+++ params/a_voyerii.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <- 4.2 
+    
+# e.g. "anas_krystallinus"    
+species.name <- ale_voyerii
+    
+# e.g. "red"    
+color <- "magenta"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
