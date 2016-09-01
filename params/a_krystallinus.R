ac083903199886d5053061d79de00e1ea0366f37
annakrystalli <annakrystalli@googlemail.com>
2016-09-01 14:00:23 -0500

    Commit parameters for a_krystallinus

---
+++ params/a_krystallinus.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <-  0.5
+    
+# e.g. "anas_krystallinus"    
+species.name <- "anas_krystallinus"
+    
+# e.g. "red"    
+color <- "#279C17"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
