eb211091b7eda773934a62fe8068aaf1b7986b55
OscarSM <o.sanchezmacouzet@gmail.com>
2016-09-01 14:18:55 -0500

    Test

---
+++ params/S_nebouxii.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <-  0.5
+  
+  # e.g. "anas_krystallinus"    
+  species.name <- "Sula_nebouxii_nebouxii"
+  
+  # e.g. "red"    
+  color <- "blue"
+  
+  # pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
