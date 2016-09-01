4d772eab1cffa89b194c94037277ec4125d0742b
Dante Jiménez <dantejm_ortega@hotmail.com>
2016-09-01 14:16:09 -0500

    Added paremeter for d_jimenus

---
+++ params/d_jimenus.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <-  4.5
+    
+# e.g. "anas_krystallinus"    
+species.name <- "dante_jimenus"
+    
+# e.g. "red"    
+color <- "green"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
