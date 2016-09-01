9c6664a576b3611af6feaa0c43964762f2a0e0f8
Taracena <vtaracena@ciencias.unam.mx>
2016-09-01 13:35:24 -0500

    Params C. taracenae
    Added parameters for C. taracenae. RStudio is still loading the git tab so I'll commit the changes from here.

---
+++ params/Curculio_taracenae.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <- 2.5  
+    
+# e.g. "anas_krystallinus"    
+species.name <- "Curculio taracenae"
+    
+# e.g. "red"    
+color <- "chartreuse3"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
