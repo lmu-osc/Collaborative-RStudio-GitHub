3fd15cb0cf93575b3ca138a2355eaea6a124fab6
LynnaKiere <lynnamariekiere@gmail.com>
2016-09-01 14:02:32 -0500

    parameters for L_kierensis

---
+++ params/L_kierensis.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <- 2.84 
+    
+# e.g. "anas_krystallinus"    
+species.name <-"Lynnus_kierensis"
+    
+# e.g. "red"    
+color <-"turquiose"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
