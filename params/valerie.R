b9d5577c3000de155e0bfd18274b2329029b4baf
valdeanda <valdeanda@ciencias.unam.mx>
2016-09-01 14:23:39 -0500

    Prueba otra vez!

---
+++ params/valerie.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <-  0.8
+    
+# e.g. "anas_krystallinus"    
+species.name <- "Escherichia coli"
+    
+# e.g. "red"    
+color <- "#CCE5FF"
+    
+# pick a color at http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
