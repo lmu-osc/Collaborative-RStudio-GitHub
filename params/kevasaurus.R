4f70ef7c3e8359ebfae51ec5758cb34eaa0ddc6b
Kevin Wojkovich <kevinw@purdue.edu>
2016-10-29 09:09:42 -0400

    adding my new species.

---
+++ params/kevasaurus.R
@@ -0,0 +1,14 @@
+# input parameters
+# ________________________________________________
+
+
+# sigma2: 0 < value < 5
+sig2 <- 2.2 
+    
+# e.g. "anas_krystallinus"    
+species.name <- "kevasaurus"
+    
+# e.g. "red"    
+color <- "orange"
+    
+# tip: pick a color at using rstudio colour picker. Addins > Colour Picker
