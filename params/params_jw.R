2e16320bfb922243c17ec9ec1f0e01dff858fb4d
Jessica Ward <jessic@Jessicas-MacBook-Air.local>
2017-05-18 11:51:27 +0100

    added jwspecies

--- params/params_jw.R
+++ params/params_jw.R
@@ -5,11 +5,11 @@
 
 # sigma2: 0 < value < 5
 sig2 <-  0.256
-    
+   
 # e.g. "anas_krystallinus"    
 species.name <- "jwspecies"
     
 # e.g. "red"    
-color <- "turquoise4"
+color <- c("#008B8B")
     
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
