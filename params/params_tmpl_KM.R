ae96cc2f65c2e2295845f62ddfbc17e7eba5b9d2
Kamini Manick <Kamini@Kaminis-MacBook-Pro.local>
2017-05-18 12:03:37 +0100

    Saved this time!

--- params/params_tmpl_KM.R
+++ params/params_tmpl_KM.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-  
+sig2 <-  1.5
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "kamini_manick"
     
 # e.g. "red"    
-color <- 
+color <- "deeppink2"
     
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
