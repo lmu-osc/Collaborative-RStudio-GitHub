222bf55ab7606af7971799baded635d31f6ffc80
Ellen Coombs <ellencoombs@Ellens-MacBook-Air.local>
2017-05-18 12:03:45 +0100

    Saved this time - changes to species

--- params/Ellen_params_tmpl.R
+++ params/Ellen_params_tmpl.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-  
+sig2 <-  4
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "ellenus_coombsus"
     
 # e.g. "red"    
-color <- 
+color <- c("#00008B")
     
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
