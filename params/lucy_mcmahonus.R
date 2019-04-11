1e28a36fa318d29402066e08d9e1ab17f43582a6
BO4VK27 BO4VK27 <bo4vk27@sheffield.ac.uk>
2019-04-11 10:48:18 +0100

    first draft

--- params/lucy_mcmahonus.R
+++ params/lucy_mcmahonus.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 2.5
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "lucy_mcmahonus"
     
 # e.g. "red"    
-color <- 
+color <- "cyan"
     
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
