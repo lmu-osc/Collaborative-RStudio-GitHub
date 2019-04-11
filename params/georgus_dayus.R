8c3f53c189db00fb9fc755b72e7217490cc51efb
gwd500 <gwd500@york.ac.uk>
2019-04-11 10:50:44 +0100

    changed sp name

--- params/georgus_dayus.R
+++ params/georgus_dayus.R
@@ -4,10 +4,10 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- "3"
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "georgus daus"
     
 # e.g. "red"    
 color <- "blue"
