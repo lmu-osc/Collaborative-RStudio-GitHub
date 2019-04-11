d51beae11b150a502e7498d97d35226df35a2c08
BO4VK17 BO4VK17 <bo4vk17@sheffield.ac.uk>
2019-04-11 10:42:01 +0100

    kelly rossus updated

--- params/kelly_rossus.R
+++ params/kelly_rossus.R
@@ -4,12 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 3.1
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "kelly_rossus"
     
 # e.g. "red"    
-color <- 
+color <- "darkorchid4"
     
 # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
+
