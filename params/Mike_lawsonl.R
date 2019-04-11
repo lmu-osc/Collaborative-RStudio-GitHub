6c4d61915beb066944de33e1a6900079c4c705b9
mrml500 <mrml500@york.ac.uk>
2019-04-11 10:41:39 +0100

    updated species parameter

--- params/Mike_lawsonl.R
+++ params/Mike_lawsonl.R
@@ -4,12 +4,12 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 2.5
+sig2 <- 4.9
     
 # e.g. "anas_krystallinus"    
 species.name <- "michaelus lawsonus"
     
 # e.g. "red"    
-color <- "deepskyblue3"
+color <- "black"
     
 # tip: pick one of a color at using rstudio colour picker. Addins > Colour Picker
