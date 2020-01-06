8d93a97f5bbafd35535723f7f2995c88d7f342e3
lackerm2 <lenaackermann@gmx.net>
2020-01-06 15:09:19 +0000

    added species parameters

---
+++ my_species.R
@@ -0,0 +1,14 @@
+
+# input sample species parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 4
+  
+  # e.g. "anas_krystallinus"    
+  species.name <- "test_species"
+  
+  # e.g. "red"    
+  color <- "cyan4"
+  
+  # tip: pick a color at using rstudio colour picker. Addins > Colour Picker
+  
