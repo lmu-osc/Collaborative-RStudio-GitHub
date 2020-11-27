5932960c298b7c86a06f89b983853119317910ba
Isabel Morgan <isabelmorgan95@gmail.com>
2020-11-27 17:23:21 +0000

    Add Centi_lizard parameters

--- params/CopyOfparams_CentiMorgan.R
+++ params/CopyOfparams_CentiMorgan.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 4.8
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "Centi_lizard"
     
 # e.g. "red"    
-color <- 
+color <- "#41CC29"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
