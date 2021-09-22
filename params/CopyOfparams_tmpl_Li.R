67a34a154430caf274b749e938b8d5f6956d741d
lucitropie <lucie-yuanting.li@t-online.de>
2021-09-22 18:09:29 +0200

    maybe now

--- params/CopyOfparams_tmpl_Li.R
+++ params/CopyOfparams_tmpl_Li.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 3.3
     
-# e.g. "anas_krystallinus"    
-species.name <- 
+# e.g. "lucie_lucieferase"    
+species.name <- "lucie_lucieferase"
     
 # e.g. "red"    
-color <- 
+color <- "#8470FF"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
