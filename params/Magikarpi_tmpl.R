0b60740352a857b6b9875a7f232d1c66e363683f
Rori Wijnhorst <roriwijnhorst@gmail.com>
2022-05-31 16:45:32 +0200

    Magikarpus_sp

--- params/Magikarpi_tmpl.R
+++ params/Magikarpi_tmpl.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 2
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- Magikarpus_pokemoni
     
 # e.g. "red"    
-color <- 
+color <- "#FF3030"
     
 # tip: pick a color using rstudio colour picker. 
-# Addins > Colour Picker (to install: install.packages("colourpicker"))
+install.packages("colourpicker")
