5fc3d5f24bd4a283f8a276bc74db429c3d7a76f8
Philippa Hammond <fighammond@gmail.com>
2022-09-08 15:55:23 +0200

    new parameters

--- params/params_tmpl.R
+++ params/params_tmpl.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 1
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "figosaur"
     
 # e.g. "red"    
-color <- 
+color <- "dark green"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
