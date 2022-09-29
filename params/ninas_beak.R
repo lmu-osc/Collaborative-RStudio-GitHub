411aa1cb7dd497f6005a313bb4573578b921b22e
Nina-Alisa Hinz <nina-alisa@gmx.de>
2022-09-29 13:27:28 +0200

    add Nina's beak parameter

---
+++ params/ninas_beak.R
@@ -0,0 +1,13 @@
+# input parameters
+
+# sigma2: 0 < value < 5
+sig2 <- 3.7
+    
+# e.g. "anas_krystallinus"    
+species.name <- "ninas_beak"
+    
+# e.g. "red"    
+color <- "#6DE082"
+    
+# tip: pick a color using rstudio colour picker. 
+# Addins > Colour Picker (to install: install.packages("colourpicker"))
