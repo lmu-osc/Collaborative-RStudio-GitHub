66db2f1f647df3ddb0f540624f1dbbcf70db5f60
Rori Wijnhorst <roriwijnhorst@gmail.com>
2022-09-29 13:21:20 +0200

    Add my species parameters. Forgot to press save.

--- params/Rori_incredibilus.R
+++ params/Rori_incredibilus.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 3.1415926
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "rori_incredibilus"
     
 # e.g. "red"    
-color <- 
+color <- "#FF47B5"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
