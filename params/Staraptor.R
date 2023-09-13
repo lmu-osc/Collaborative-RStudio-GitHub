b056da00e888e2f2207ec464151b38b354ec1fca
Jonas Huber <jhuber.uhg@gmail.com>
2023-09-13 15:19:54 +0200

    added and saved qualities of the bird

--- params/Staraptor.R
+++ params/Staraptor.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 3.88836363
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "jonas_staraptor"
     
 # e.g. "red"    
-color <- 
+color <- "magenta"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
