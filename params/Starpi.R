d29bbabd676d33b0581f7dcaf87f01e03915546a
Johannes Starp <johannes.starp@outlook.com>
2023-09-13 15:33:08 +0200

    Starpis Parameter

--- params/Starpi.R
+++ params/Starpi.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
-    
+sig2 <- 2
+
 # e.g. "anas_krystallinus"    
-species.name <- 
-    
+species.name <- "Starpi"
+
 # e.g. "red"    
-color <- 
-    
+color <- c("#12E68E")
+
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
