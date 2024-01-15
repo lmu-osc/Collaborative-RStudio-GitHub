926abfe426ba2e7fbf5e8a77678755447e45a9ca
Elisabeth M <e.moulaert@campus.lmu.de>
2024-01-15 14:43:00 +0000

    hopefully params actually there this time for elisaurus

--- params/Elisaurus.R
+++ params/Elisaurus.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 
+sig2 <- 4.2
     
 # e.g. "anas_krystallinus"    
-species.name <- 
+species.name <- "elisaurus"
     
 # e.g. "red"    
-color <- 
+color <- "#5CACEE"
     
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
