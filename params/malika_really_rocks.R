22ee68edce9f0cdc36effd815d8283f243bbf45f
MWiehr <122365830+MWiehr@users.noreply.github.com>
2024-05-24 12:22:02 +0200

    added new pictures to match  the updated repository

--- params/malika_really_rocks.R
+++ params/malika_really_rocks.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <- 3.5
+sig2 <- 3.2
     
 # e.g. "anas_krystallinus"    
-species.name <- "malikus_rocks"
+species.name <- "not_malika"
     
 # e.g. "red"    
-color <- "311DB3"
-    
+color <- "green"
+
 # tip: pick a color using rstudio colour picker. 
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
