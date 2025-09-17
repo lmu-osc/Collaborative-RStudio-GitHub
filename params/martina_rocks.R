154435101f98dce843566cf3ce4976ccac6b7bfa
Martina Langhals <martinalanghals225@gmail.com>
2025-09-17 17:01:30 +0200

    add Martina rocks parameters

--- params/martina_rocks.R
+++ params/martina_rocks.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-
+sig2 <-2
 
 # e.g. "anas_krystallinus"
-species.name <-
+species.name <-"anas_krystallinus"
 
 # e.g. "red"
-color <-
+color <- "blue"
 
 # tip: pick a color using rstudio colour picker.
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
