748542906d4528f8d901fcb72c41723ecc2a7ab7
Asmaa_Alselwi <asmaa.alselwi@helmholtz-munich.de>
2025-09-17 17:28:58 +0200

    Added Asma's parameters

--- params/Asma_Alselwi.R
+++ params/Asma_Alselwi.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-
+sig2 <- 2.5
 
 # e.g. "anas_krystallinus"
-species.name <-
+species.name <- "asma alselwi"
 
 # e.g. "red"
-color <-
+color <- "red"
 
 # tip: pick a color using rstudio colour picker.
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
