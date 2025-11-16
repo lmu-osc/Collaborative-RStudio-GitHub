dcb1561718f0a8051bf45affe87d055da8c4f530
Ivana Kapounova <kap.ivca@seznam.cz>
2025-11-16 23:45:59 +0100

    add params

--- params/slimp.R
+++ params/slimp.R
@@ -4,13 +4,13 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-
+sig2 <- 4.1
 
 # e.g. "anas_krystallinus"
-species.name <-
+species.name <- "slimpiv"
 
 # e.g. "red"
-color <-
+color <- "#9ACD32"
 
 # tip: pick a color using rstudio colour picker.
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
