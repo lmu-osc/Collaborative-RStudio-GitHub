0ac7a6398fe46e9293ff253236e47083d21742b3
Patrick Callahan <patricktcallahan18@gmail.com>
2026-07-21 12:18:46 +0200

    Stop auto install in R script

--- params/Franzi_rocks_a_little.R
+++ params/Franzi_rocks_a_little.R
@@ -1,7 +1,7 @@
 # !!copied version!! editable :)
 # ________________________________________________
 # input parameters
-install.packages("colourpicker")
+# install.packages("colourpicker")
 
 # sigma2: 0 < value < 5
 sig2 <- 4
