3b4a229114120b9f0fb9f253ae78ec1ab1a7ea2f
tolga-data-studies <tolga.t.sahin@gmail.com>
2026-09-09 15:19:58 +0200

    my params

--- params/tolga_s.R
+++ params/tolga_s.R
@@ -4,18 +4,18 @@
 # input parameters
 
 # sigma2: 0 < value < 5
-sig2 <-
+sig2 <- 3
 
 # e.g. "anas_krystallinus"
-species.name <-
+species.name <- "tolga try"
 
 # e.g. "red"
-color <-
+color <- "#00CED1"
 
 # tip: pick a color using rstudio colour picker.
 # Addins > Colour Picker (to install: install.packages("colourpicker"))
   # Note: please do not include install.packages("PKG-NAME") in your final R script!
-  # This can break part of our rendering pipeline. Please install at your command 
+  # This can break part of our rendering pipeline. Please install at your command
   # line or simply ask your instructor if uncertain.
 # If you use this, don't forget to put it back as a comment
 # before pushing and creating your pull request.
